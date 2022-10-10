910e037c139fd253bb9541285e25bf9fe23dac50
adhi3012 <68736986+adhi3012@users.noreply.github.com>
2022-10-10 19:59:57 +0530

    Update streamlit_app.py
    added extra features

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
-
+🥣 🥗 🐔 🥑🍞
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('kale. Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔kale. Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
